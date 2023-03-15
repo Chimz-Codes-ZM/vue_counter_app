b0b5094bb0d0cde16a99c242c963a940b3021751
Chimwemwe Masona <justplainrodney@gmail.com>
2023-03-15 16:09:15 +0200

    completed the basics

--- counter/src/App.vue
+++ counter/src/App.vue
@@ -1,18 +1,46 @@
 <template>
   <div>
     <center>
-      <h1>Counter: </h1>
-      <button>Increment</button>
-      <button>Decrement</button>
-      <button>Reset</button>
+      <h1>Counter: {{ counter }}</h1>
+      <button @click="increment">Increment</button>
+      <button @click="decrement">Decrement</button>
+      <button @click="reset">Reset</button>
+      <input type="number" v-model="value">
+      <button @click="setValue(value)">Set to Value</button>
     </center>
   </div>
 </template>
 
 <script>
 
-export default {
 
+export default {
+computed: {
+ counter(){
+  return this.$store.getters.counter
+ }
+},
+
+methods: {
+  increment() {
+    this.$store.dispatch('increment')
+  },
+  decrement() {
+    this.$store.dispatch('decrement')
+  },
+  reset(){
+    this.$store.dispatch('reset')
+  },
+  setValue(value) {
+    this.$store.dispatch("setValue", parseInt(value))
+  }
+},
+
+data() {
+  return {
+    value: "",
+  };
+}
   
 }
 </script>
---
+++ counter/src/store.js
@@ -0,0 +1,48 @@
+import { createStore } from "vuex";
+
+const store=createStore({
+    state: {
+        count: 0
+    },
+
+    getters: {
+        counter(state){
+            return state.count
+        }
+    },
+
+    actions: {
+        increment({commit}){
+            commit('increment')
+        },
+
+        decrement({commit}){
+            commit('decrement')
+        },
+
+        reset({commit}){
+            commit('reset')
+        },
+
+        setValue({ commit }, payload){
+            commit("setValue", payload)
+        }
+    },
+
+    mutations: {
+        increment(state){
+            state.count++
+        },
+        decrement(state){
+            state.count--
+        },
+        reset(state){
+            state.count = 0
+        },
+        setValue(state, payload) {
+            state.count = payload;
+        }
+    }
+})
+
+export default store
